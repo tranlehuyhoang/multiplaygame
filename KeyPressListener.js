998bf37f9c414dc0fa17f32ba1003586202e5034
Huyh Huyhu <2509roblox@gmail.com>
2023-03-26 14:32:46 +0700

    lafafwe

--- KeyPressListener.js
+++ KeyPressListener.js
@@ -1,27 +1,35 @@
 class KeyPressListener {
-  constructor(keyCode, callback) {
-    let keySafe = true;
-    this.keydownFunction = function(event) {
-      if (event.code === keyCode) {
-         if (keySafe) {
-            keySafe = false;
-            callback();
-         }  
-      }
-   };
-   this.keyupFunction = function(event) {
-      if (event.code === keyCode) {
-         keySafe = true;
-      }         
-   };
-   document.addEventListener("keydown", this.keydownFunction);
-   document.addEventListener("keyup", this.keyupFunction);
-  }
+   constructor(keyCode, callback) {
+      let keySafe = true;
+      this.keydownFunction = function (event) {
+         if (event.code === keyCode) {
+            if (keySafe) {
+               keySafe = false;
+               callback();
+            }
+         }
+      };
+      this.keyupFunction = function (event) {
+         console.log(keyCode)
+         if (event.code === keyCode) {
+            keySafe = true;
+         }
+      };
+      console.log('object')
+      document.addEventListener("keydown", this.keydownFunction);
+      document.addEventListener("keyup", this.keyupFunction);
+      // document.querySelector('.len').addEventListener("click", this.keydownFunction);
+      // document.querySelector('.len').addEventListener("click", this.keyupFunction);
+   }
 
-  unbind() { 
-    document.removeEventListener("keydown", this.keydownFunction);
-    document.removeEventListener("keyup", this.keyupFunction);
-  }
+   unbind() {
+      document.removeEventListener("keydown", this.keydownFunction);
+      document.removeEventListener("keyup", this.keyupFunction);
+   }
 
 
 }
+function hehe() {
+   console.log('ehhe')
+}
+document.addEventListener("keydown", hehe())
